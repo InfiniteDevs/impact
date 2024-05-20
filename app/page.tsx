7cce6f126bb16ccda47369c60fd001c494f5755a
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-05-20 18:58:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,11 @@ import { HeroParallax } from "@/components/ui/hero-parallax";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 
 export default function Home() {
-  
   return (
     <>
-      
       <HeroParallax products={products} />
       <div className="h-[40rem] flex justify-center items-center px-4 ">
         <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-          Build
-          <FlipWords words={words} /> <br />
           Customer reivews
         </div>
         <InfiniteMovingCards
