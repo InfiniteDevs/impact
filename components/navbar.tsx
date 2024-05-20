14cd806021c9a515ae1276cad79bcefdcfe3cc65
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-05-20 18:57:11 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,18 +20,8 @@ export default function Navbar({ className }: { className?: string }) {
         <Menu setActive={setActive}>
           <HoveredLink href="/">Home</HoveredLink>
           <HoveredLink href="/pricing">Pricing</HoveredLink>
-          <MenuItem setActive={setActive} active={active} item="Services">
-            <div className="flex flex-col space-y-4 text-sm">
-              <HoveredLink href="/web-dev">Web Development</HoveredLink>
-              <HoveredLink href="/interface-design">
-                Interface Design
-              </HoveredLink>
-              <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-              <HoveredLink href="/branding">Branding</HoveredLink>
-            </div>
-          </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Products">
-            <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+          <MenuItem setActive={setActive} active={active} item="Projects">
+            <div className="text-sm grid grid-cols-2 gap-10 p-4">
               <ProductItem
                 title="Algochurn"
                 href="https://algochurn.com"
