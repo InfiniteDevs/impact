18dca9de5354324e429dad202c2a715d4ddb4d92
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-05-21 11:09:43 +0530

    Create page.tsx

---
+++ app/showcase/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import { cn } from "@/utils/cn";
+import React from "react";
+import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-[20rem]">
+        <p className="text-neutral-600 dark:text-neutral-200 text-xl sm:text-base  ">
+          Previous projects we have made.
+        </p>
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+          <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+            Join now
+          </button>
+          <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+            Signup
+          </button>
+        </div>
+      </div>
+      <BentoGrid className="max-w-4xl mx-auto">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          icon={item.icon}
+          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+        />
+      ))}
+    </BentoGrid>
+    </>
+  );
+}
+
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
+const items = [
+  {
+    title: "The Dawn of Innovation",
+    description: "Explore the birth of groundbreaking ideas and inventions.",
+    header: <Skeleton />,
+  },
+  {
+    title: "The Digital Revolution",
+    description: "Dive into the transformative power of technology.",
+    header: <Skeleton />,
+  },
+  {
+    title: "The Art of Design",
+    description: "Discover the beauty of thoughtful and functional design.",
+    header: <Skeleton />,
+  },
+  {
+    title: "The Power of Communication",
+    description:
+      "Understand the impact of effective communication in our lives.",
+    header: <Skeleton />,
+  },
+  {
+    title: "The Pursuit of Knowledge",
+    description: "Join the quest for understanding and enlightenment.",
+    header: <Skeleton />,
+  },
+  {
+    title: "The Joy of Creation",
+    description: "Experience the thrill of bringing ideas to life.",
+    header: <Skeleton />,
+  },
+  {
+    title: "The Spirit of Adventure",
+    description: "Embark on exciting journeys and thrilling discoveries.",
+    header: <Skeleton />,
+  },
+];
+
