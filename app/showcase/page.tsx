8f5cd96b42cbb200bf02e369dbcbc09b9778a148
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-05-21 13:01:03 +0530

    Update page.tsx

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center h-[20rem]">
-        <p className="text-neutral-600 dark:text-neutral-200 text-4xl font-mono">
+        <p className="text-neutral-600 dark:text-neutral-200 text-4xl font-mono pb-4">
           Previous projects we have made.
         </p>
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
